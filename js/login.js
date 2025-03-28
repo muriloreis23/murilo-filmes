37eb812c1b700f831e77d7dce31809187747a62c
muriloreis23 <muriloreis060384@outlook.com>
2025-03-27 22:16:47 -0300

    pagina 45

---
+++ js/login.js
@@ -0,0 +1,6 @@
+// Ao carregar a página executaras funções de buscar os dados 
+document.addEventListener("DOMContentLoaded",async () => {
+    setTimeout(() => {
+        toggleLoading();
+    }, 2000);
+});  
